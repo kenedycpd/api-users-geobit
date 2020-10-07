3bf53043d51ddcf4bb1a3dfc6ef6f398428333ba
kenedy <kenedycelestino89@gmail.com>
2020-10-07 11:44:16 -0300

    create test models users

--- usuarios/tests.py
+++ usuarios/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Users
 
-# Create your tests here.
+
+class UsersTest(TestCase):
+    def setUp(self):
+        Users.objects.create(
+            username='kenedy',
+            email='kenedy@kenedy.com',
+            password=123
+        )
+
+    def test_model_users(self):
+        user = Users.objects.get(username='kenedy')
+        self.assertEquals(user.__str__(), 'kenedy')
