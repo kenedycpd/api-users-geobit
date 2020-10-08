041ede32fcd75ad8e9767d623a25fa3095acdc5d
kenedy <kenedycelestino89@gmail.com>
2020-10-08 11:24:38 -0300

    Add JWt

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,4 @@
+from rest_framework_jwt.views import obtain_jwt_token
 from django.contrib import admin
 from django.urls import include, path
 from usuarios import api
@@ -5,5 +6,6 @@ from usuarios import api
 urlpatterns = [
     path('', api.home, name='home'),
     path('usuarios/', include('usuarios.urls')),
+    path('usuarios/', obtain_jwt_token),
     path('admin/', admin.site.urls),
 ]
